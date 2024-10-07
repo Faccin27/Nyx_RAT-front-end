f2c63e40ec4b3d792ab76e597594f017b81a9ab9
Guilherme Faccin <gfaccin27@gmail.com>
2024-10-07 14:42:24 -0300

    feat: user registration working correctly from front-end

--- src/components/loginform.tsx
+++ src/components/loginform.tsx
@@ -1,11 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Link from "next/link";
+import axios from "axios";
 
 function LoginRegisterForm() {
   const [isLoginForm, setIsLoginForm] = useState(true);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    phone: "",
+    birthday: "",
+    gender: "",
+  });
 
   const toggleForm = () => {
     setIsLoginForm(!isLoginForm);
+  };
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:3001/users/", {
+        name: formData.username,
+        password: formData.password,
+        email: formData.email,
+        photo: null,
+        registeredDate: new Date().toISOString(),
+        expiryDate: new Date(
+          new Date().setFullYear(new Date().getFullYear() + 1)
+        ).toISOString(),
+        gender: formData.gender === "male" ? "Masculino" : "Feminino",
+        birthDate: new Date(formData.birthday).toISOString(),
+        phone: formData.phone,
+        role: "USER",
+      });
+
+      console.log("User registered successfully:", response.data);
+      // Aqui sera o modal de usuario registrado com sucesso
+    } catch (error) {
+      console.error("Error registering user:", error);
+      // Aqui um modal seguindo o mesmo padrão para apresentar o erro.
+    }
   };
 
   return (
@@ -24,7 +70,7 @@ function LoginRegisterForm() {
               className="w-full px-3 py-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
-          
+
           <div>
             <label htmlFor="password" className="block text-white mb-1">
               Password
@@ -52,7 +98,7 @@ function LoginRegisterForm() {
               Forgot password?
             </Link>
           </div>
-        
+
           <button
             type="submit"
             className="w-full bg-white hover:bg-purple-700 text-black py-2 rounded-md transition-all duration-300 ease-in-out transform hover:scale-105"
@@ -61,82 +107,88 @@ function LoginRegisterForm() {
           </button>
         </form>
       ) : (
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleRegister}>
           <div className="grid grid-cols-2 gap-4">
             <div>
-              <label htmlFor="reg-username" className="block text-white mb-1">
+              <label htmlFor="username" className="block text-white mb-1">
                 Username
               </label>
               <input
-                id="reg-username"
-                name="reg-username"
+                id="username"
+                name="username"
                 type="text"
                 placeholder="Username"
                 className="w-full px-3 py-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={handleInputChange}
               />
             </div>
             <div>
-              <label htmlFor="reg-email" className="block text-white mb-1">
+              <label htmlFor="email" className="block text-white mb-1">
                 Email
               </label>
               <input
-                id="reg-email"
-                name="reg-email"
+                id="email"
+                name="email"
                 type="email"
                 placeholder="Email"
                 className="w-full px-3 py-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={handleInputChange}
               />
             </div>
             <div>
-            <label htmlFor="reg-phone" className="block text-white mb-1">
-              Phone
-            </label>
-            <input
-              id="reg-phone"
-              name="reg-phone"
-              type="tel"
-              placeholder="Enter your phone number"
-              className="w-full px-3 py-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div>
-            <label htmlFor="reg-password" className="block text-white mb-1">
-              Password
-            </label>
-            <input
-              id="reg-password"
-              name="reg-password"
-              type="password"
-              placeholder="Enter your password"
-              className="w-full px-3 py-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+              <label htmlFor="phone" className="block text-white mb-1">
+                Phone
+              </label>
+              <input
+                id="phone"
+                name="phone"
+                type="tel"
+                placeholder="Enter your phone number"
+                className="w-full px-3 py-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={handleInputChange}
+              />
+            </div>
             <div>
-              <label htmlFor="reg-birthday" className="block text-white mb-1">
+              <label htmlFor="password" className="block text-white mb-1">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Enter your password"
+                className="w-full px-3 py-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="birthday" className="block text-white mb-1">
                 Birthday
               </label>
               <input
-                id="reg-birthday"
-                name="reg-birthday"
+                id="birthday"
+                name="birthday"
                 type="date"
                 className="w-full px-3 py-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={handleInputChange}
               />
             </div>
             <div>
-              <label htmlFor="reg-gender" className="block text-white mb-1">
+              <label htmlFor="gender" className="block text-white mb-1">
                 Gender
               </label>
               <select
-                id="reg-gender"
-                name="reg-gender"
+                id="gender"
+                name="gender"
                 className="w-full px-3 py-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onChange={handleInputChange}
               >
                 <option value="">Select</option>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
               </select>
             </div>
-            </div>
+          </div>
           <div className="flex items-center space-x-2">
             <input
               type="checkbox"
